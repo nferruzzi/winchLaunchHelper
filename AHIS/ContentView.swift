041051a0921fc9525c0b4292a11554da3fa44118
Nicola Ferruzzi <nicola.ferruzzi+github@gmail.com>
2021-01-09 17:59:06 +0100

    Multiple views

--- AHIS/ContentView.swift
+++ AHIS/ContentView.swift
@@ -49,9 +49,6 @@ struct TriangleShape: Shape {
 
 struct BackgroundView2: View {
     enum Constants {
-        static let aspectRatio: CGFloat = 0.4
-        static let height: CGFloat = 1024 * aspectRatio
-        static let width: CGFloat = 1024 * aspectRatio
         static let blueI = Color(red: 0.47, green: 0.66, blue: 0.82)
         static let blueO = Color(red: 0.04, green: 0.35, blue: 0.53)
         static let brownI = Color(red: 0.36, green: 0.27, blue: 0.24)
@@ -61,45 +58,43 @@ struct BackgroundView2: View {
         static let earthGradient = Gradient(colors: [brownO, brownI])
         static let earthGradientO = Gradient(colors: [brownI, brownO])
         static let fov: CGFloat = 120
-        static let degreeToPixel = height / fov
-        static func pitchToPixel(_ pitch: CGFloat) -> CGFloat {
-            height / 2.0 + pitch * degreeToPixel
-        }
     }
 
     struct SkyShape: Shape {
-        var pitch: CGFloat
+        let size: CGFloat
+        var horizont: CGFloat
 
         func path(in rect: CGRect) -> Path {
             Path { path in
                 path.move(to: CGPoint(x: 0, y: 0))
-                path.addLine(to: CGPoint(x: Constants.width, y: 0))
-                path.addLine(to: CGPoint(x: Constants.width, y: Constants.pitchToPixel(pitch)))
-                path.addLine(to: CGPoint(x: 0, y: Constants.pitchToPixel(pitch)))
+                path.addLine(to: CGPoint(x: size, y: 0))
+                path.addLine(to: CGPoint(x: size, y: horizont))
+                path.addLine(to: CGPoint(x: 0, y: horizont))
             }
         }
         
         var animatableData: CGFloat {
-            get { pitch }
-            set { pitch = newValue }
+            get { horizont }
+            set { horizont = newValue }
         }
     }
     
     struct EarthShape: Shape {
-        var pitch: CGFloat
+        let size: CGFloat
+        var horizont: CGFloat
 
         func path(in rect: CGRect) -> Path {
             Path { path in
-                path.move(to: CGPoint(x: 0, y: Constants.pitchToPixel(pitch)))
-                path.addLine(to: CGPoint(x: Constants.width, y: Constants.pitchToPixel(pitch)))
-                path.addLine(to: CGPoint(x: Constants.width, y: Constants.height))
-                path.addLine(to: CGPoint(x: 0, y: Constants.height))
+                path.move(to: CGPoint(x: 0, y: horizont))
+                path.addLine(to: CGPoint(x: size, y: horizont))
+                path.addLine(to: CGPoint(x: size, y: size))
+                path.addLine(to: CGPoint(x: 0, y: size))
             }
         }
         
         var animatableData: CGFloat {
-            get { pitch }
-            set { pitch = newValue }
+            get { horizont }
+            set { horizont = newValue }
         }
     }
     
@@ -114,7 +109,7 @@ struct BackgroundView2: View {
         }
         .font(.system(size: 10))
         .foregroundColor(.white)
-        .offset(x: 0, y: ((outer ? 0 : pitch) + offset) * Constants.degreeToPixel)
+        .offset(x: 0, y: ((outer ? 0 : pitch) + offset) * degreeToPixel)
     }
     
     func pin<Content: View>(_ angle: CGFloat, _ content: @autoclosure () -> Content) -> some View {
@@ -123,17 +118,19 @@ struct BackgroundView2: View {
         
         return content()
             .rotationEffect(.degrees(Double(-angle)), anchor: UnitPoint(x: 0.5, y: 0.8))
-            .offset(x: sin(piAngle) * Constants.width/2.0, y: cos(piAngle) * Constants.height/2.0)
+            .offset(x: sin(piAngle) * size/2.0, y: cos(piAngle) * size/2.0)
     }
         
     var background: some View {
         ZStack {
-            SkyShape(pitch: outer ? 0.0 : pitch)
-            .fill(LinearGradient(gradient: Constants.skyGradient, startPoint: .top, endPoint: .bottom))
+            SkyShape(size: size, horizont: outer ? pitchToPixel(0) : pitchToPixel(pitch))
+                .fill(LinearGradient(gradient: Constants.skyGradient, startPoint: .top, endPoint: .bottom))
+                .frame(width: size, height: size)
 
-            EarthShape(pitch: outer ? 0.0 : pitch)
-            .fill(LinearGradient(gradient: Constants.earthGradient, startPoint: .bottom, endPoint: .top))
-            
+            EarthShape(size: size, horizont: outer ? pitchToPixel(0) : pitchToPixel(pitch))
+                .fill(LinearGradient(gradient: Constants.earthGradient, startPoint: .bottom, endPoint: .top))
+                .frame(width: size, height: size)
+
             if !outer {
                 line(20, "20", 100)
                 line(15, "", 50)
@@ -145,8 +142,8 @@ struct BackgroundView2: View {
                 line(-5, "", 50)
             }
         }
-        .frame(width: Constants.width, height: Constants.height)
-        .overlay(line(0, "", Constants.width).foregroundColor(.white))
+        .aspectRatio(CGSize(width: 1, height: 1), contentMode: .fill)
+        .overlay(line(0, "", size).foregroundColor(.white))
         .overlay(pin(0, TriangleShape(offset: 0).frame(width: 30, height: 35).foregroundColor(.white)))
         .overlay(pin(45, TriangleShape(offset: 0).frame(width: 15, height: 20).offset(x: 0, y: -17).foregroundColor(.white)))
         .overlay(pin(-45, TriangleShape(offset: 0).frame(width: 15, height: 20).offset(x: 0, y: -17).foregroundColor(.white)))
@@ -162,10 +159,24 @@ struct BackgroundView2: View {
         .drawingGroup()
     }
     
+    let width: CGFloat
+    let height: CGFloat
     let pitch: CGFloat
     let roll: CGFloat
     let yaw: CGFloat
     let outer: Bool
+    
+    var degreeToPixel: CGFloat {
+        size / Constants.fov
+    }
+
+    func pitchToPixel(_ pitch: CGFloat) -> CGFloat {
+        size / 2.0 + pitch * degreeToPixel
+    }
+
+    var size: CGFloat {
+        min(width, height)
+    }
 
     var body: some View {
         background
@@ -178,7 +189,7 @@ extension Double {
     }
 }
 
-struct ContentView: View {
+struct AttitudeIndicatorView: View {
     let sim: Bool
     
     @StateObject var model = AHServiceViewModel()
@@ -213,40 +224,68 @@ struct ContentView: View {
     var body: some View {
         VStack {
             ZStack {
-                BackgroundView2(pitch: sim ? pitch : CGFloat(model.pitch),
-                                roll: CGFloat(model.roll),
-                                yaw: sim ? yaw : CGFloat(model.yaw),
-                                outer: false)
-                    .animation(.linear)
-                    .mask(Circle())
-                    .overlay(plane.shadow(radius: 5), alignment: .center)
-                    .overlay(virata.shadow(radius: 5), alignment: .top)
-                
-                BackgroundView2(pitch: sim ? pitch : CGFloat(model.pitch),
-                                roll: CGFloat(model.roll),
-                                yaw: sim ? yaw : CGFloat(model.yaw),
-                                outer: true)
-                    .animation(.linear)
-                    .mask(Circle().strokeBorder(style: StrokeStyle.init(lineWidth: 40, lineCap: .round, lineJoin: .round)))
-                    .shadow(color: Color(.sRGBLinear, white: 1, opacity: 0.7), radius: 5)
-
+                GeometryReader { geometry in
+                    BackgroundView2(width: geometry.size.width,
+                                    height: geometry.size.height,
+                                    pitch: sim ? pitch : CGFloat(model.pitch),
+                                    roll: CGFloat(model.roll),
+                                    yaw: sim ? yaw : CGFloat(model.yaw),
+                                    outer: false)
+                        .animation(.linear)
+                        .mask(Circle())
+                        .overlay(plane.shadow(radius: 5), alignment: .center)
+                        .overlay(virata.shadow(radius: 5), alignment: .top)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+                    
+                    BackgroundView2(width: geometry.size.width,
+                                    height: geometry.size.height,
+                                    pitch: sim ? pitch : CGFloat(model.pitch),
+                                    roll: CGFloat(model.roll),
+                                    yaw: sim ? yaw : CGFloat(model.yaw),
+                                    outer: true)
+                        .animation(.linear)
+                        .mask(Circle().strokeBorder(style: StrokeStyle.init(lineWidth: 40, lineCap: .round, lineJoin: .round)))
+                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+                        .shadow(color: Color(.sRGBLinear, white: 1, opacity: 0.7), radius: 5)
+                }
+                .padding()
             }
-            VStack {
-//                Text("Roll \(model.roll)")
-                Text("Pitch \(sim ? Int(pitch) : model.pitch)")
-                Text("Yaw \(sim ? Int(yaw) : model.yaw)")
+            if sim {
+                VStack {
+    //                Text("Roll \(model.roll)")
+                    Text("Pitch \(sim ? Int(pitch) : model.pitch)")
+                    Text("Yaw \(sim ? Int(yaw) : model.yaw)")
+                    Button("Reset") {
+                        model.reset()
+                    }
+                    if sim {
+                        Slider(value:$pitch, in: -180...180, label: { Text("Pitch") })
+                            .frame(width: 200)
+                        Slider(value:$yaw, in: -180...180, label: { Text("Yaw") })
+                            .frame(width: 200)
+                    }
+                }
+            } else {
                 Button("Reset") {
                     model.reset()
-                }
-                if sim {
-                    Slider(value:$pitch, in: -180...180, label: { Text("Pitch") })
-                        .frame(width: 200)
-                    Slider(value:$yaw, in: -180...180, label: { Text("Yaw") })
-                        .frame(width: 200)
                 }
             }
         }
     }
+}
+
+
+struct ContentView: View {
+    let sim: Bool
+    
+    
+    var body: some View {
+        VStack {
+            AttitudeIndicatorView(sim: sim)
+            AttitudeIndicatorView(sim: sim)
+        }
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
