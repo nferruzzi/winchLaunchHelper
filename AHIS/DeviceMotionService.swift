7ee4072c65cc0fb39d4d5903b11a74159998e052
Nicola Ferruzzi <nicola.ferruzzi+github@gmail.com>
2023-08-01 18:12:45 +0200

    Add CoreLocation speed

--- AHIS/DeviceMotionService.swift
+++ AHIS/DeviceMotionService.swift
@@ -9,6 +9,7 @@ import Foundation
 import Combine
 import CoreMotion
 import UIKit
+import CoreLocation
 
 
 public protocol DeviceMotionProtocol {
@@ -17,13 +18,15 @@ public protocol DeviceMotionProtocol {
     var roll: AnyPublisher<Double, Never> { get }
     var pitch: AnyPublisher<Double, Never> { get }
     var heading: AnyPublisher<Double, Never> { get }
+    var speed: AnyPublisher<CLLocationSpeed, Never> { get }
 }
 
 
-public class DeviceMotionService: DeviceMotionProtocol {
+public class DeviceMotionService: NSObject {
     
     enum Constants {
         static let manager = CMMotionManager()
+        static let locationManager = CLLocationManager()
         static let queue = OperationQueue()
     }
 
@@ -32,6 +35,7 @@ public class DeviceMotionService: DeviceMotionProtocol {
     @Published private var rollSubject: Double = 0
     @Published private var pitchSubject: Double = 0
     @Published private var interfaceOrientation: UIInterfaceOrientation = .portrait
+    @Published private var speedSubject: Double = 0
 
     private var subscriptions = Set<AnyCancellable>()
     private var referenceAttitude: CMAttitude?
@@ -39,45 +43,16 @@ public class DeviceMotionService: DeviceMotionProtocol {
     private var rotate: Double = 10
     private var prevHeading: Double = 0
     
-//    public var deviceMotion: AnyPublisher<CMDeviceMotion?, Never> {
-//        $deviceMotionSubject.removeDuplicates().eraseToAnyPublisher()
-//    }
-
-    public var heading: AnyPublisher<Double, Never> {
-        Publishers.CombineLatest($interfaceOrientation.removeDuplicates(),
-                                 $headingSubject.removeDuplicates() )
-            .map { orientation, heading in
-                switch orientation {
-                case .portrait:
-                    return heading
-
-                case .portraitUpsideDown:
-                    return heading
-
-                case .landscapeLeft:
-                    return heading - 90
-
-                case .landscapeRight:
-                    return heading + 90
-
-                default:
-                    return heading
-                }
-            }
-            .eraseToAnyPublisher()
-    }
-
-    public var roll: AnyPublisher<Double, Never> {
-        $rollSubject.removeDuplicates().eraseToAnyPublisher()
-    }
-
-    public var pitch: AnyPublisher<Double, Never> {
-        $pitchSubject.removeDuplicates().eraseToAnyPublisher()
-    }
-
-    public init() {
+    
+    public override init() {
+        super.init()
         Constants.manager.showsDeviceMovementDisplay = true
-        Constants.manager.deviceMotionUpdateInterval = TimeInterval(1.0/25.0)
+        Constants.manager.deviceMotionUpdateInterval = TimeInterval(1.0/100.0)
+
+        Constants.locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        Constants.locationManager.activityType = .airborne
+        Constants.locationManager.pausesLocationUpdatesAutomatically = false
+        Constants.locationManager.delegate = self
         
         NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)
             .compactMap { _ in
@@ -87,6 +62,10 @@ public class DeviceMotionService: DeviceMotionProtocol {
             .store(in: &subscriptions)
 
         start(reference: .xMagneticNorthZVertical)
+        
+        if Constants.locationManager.authorizationStatus == .notDetermined {
+            Constants.locationManager.requestWhenInUseAuthorization()
+        }
         
         Publishers.CombineLatest($interfaceOrientation.removeDuplicates(),
                                  $deviceMotionSubject.compactMap { $0?.attitude } )
@@ -146,6 +125,45 @@ public class DeviceMotionService: DeviceMotionProtocol {
             self.deviceMotionSubject = motion
         }
     }
+}
+
+
+extension DeviceMotionService: DeviceMotionProtocol {
+    public var heading: AnyPublisher<Double, Never> {
+        Publishers.CombineLatest($interfaceOrientation.removeDuplicates(),
+                                 $headingSubject.removeDuplicates() )
+            .map { orientation, heading in
+                switch orientation {
+                case .portrait:
+                    return heading
+
+                case .portraitUpsideDown:
+                    return heading
+
+                case .landscapeLeft:
+                    return heading - 90
+
+                case .landscapeRight:
+                    return heading + 90
+
+                default:
+                    return heading
+                }
+            }
+            .eraseToAnyPublisher()
+    }
+
+    public var roll: AnyPublisher<Double, Never> {
+        $rollSubject.removeDuplicates().eraseToAnyPublisher()
+    }
+
+    public var pitch: AnyPublisher<Double, Never> {
+        $pitchSubject.removeDuplicates().eraseToAnyPublisher()
+    }
+
+    public var speed: AnyPublisher<Double, Never> {
+        $speedSubject.removeDuplicates().eraseToAnyPublisher()
+    }
     
     public func reset() {
         referenceAttitude = latestAttitude?.copy() as? CMAttitude
@@ -153,16 +171,52 @@ public class DeviceMotionService: DeviceMotionProtocol {
 }
 
 
-public class AHServiceViewModel: ObservableObject {
+extension DeviceMotionService: CLLocationManagerDelegate {
+    public func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        guard manager.authorizationStatus != .denied && manager.authorizationStatus != .notDetermined else { return }
+        print("auth", manager.authorizationStatus.rawValue)
+        
+        if manager.accuracyAuthorization == .reducedAccuracy {
+            Task { @MainActor in
+                do {
+                    try await Constants.locationManager.requestTemporaryFullAccuracyAuthorization(withPurposeKey: "AccurateSpeed")
+                } catch {
+                    print("full accuracy", error)
+                }
+                manager.startUpdatingLocation()
+
+            }
+        } else {
+            manager.startUpdatingLocation()
+        }
+    }
+    
+    public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let last = locations.last(where: { $0.speedAccuracy > 0 }) else { return }
+        speedSubject = last.speed
+    }
+    
+    public func locationManagerShouldDisplayHeadingCalibration(_ manager: CLLocationManager) -> Bool {
+        true
+    }
+    
+    public func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("fail", manager.authorizationStatus.rawValue, error)
+    }
+}
+
+
+public final class AHServiceViewModel: ObservableObject {
     private let ahService: DeviceMotionProtocol
     private var subscriptions = Set<AnyCancellable>()
     
     @Published public private(set) var roll: Int = 0
     @Published public private(set) var pitch: Int = 0
     @Published public private(set) var heading: Double = 0
+    @Published public private(set) var speed: Double = 0
 
-    init() {
-        ahService = DeviceMotionService()
+    init(ahService: DeviceMotionProtocol = DeviceMotionService()) {
+        self.ahService = ahService
         
         ahService
             .roll
@@ -181,6 +235,13 @@ public class AHServiceViewModel: ObservableObject {
         ahService.heading
             .receive(on: DispatchQueue.main)
             .assign(to: \.heading, on: self)
+            .store(in: &subscriptions)
+        
+        ahService
+            .speed
+            .receive(on: DispatchQueue.main)
+            .print()
+            .assign(to: \.speed, on: self)
             .store(in: &subscriptions)
     }
     
