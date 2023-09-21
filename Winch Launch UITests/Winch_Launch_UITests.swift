3c8e006d3e7b2771fd56d081ece1edef64cbee81
Nicola Ferruzzi <nicola.ferruzzi+github@gmail.com>
2023-09-21 16:41:57 +0200

    Fastlane: 1st attempt at screenshots generation

--- Winch Launch UITests/Winch_Launch_UITests.swift
+++ Winch Launch UITests/Winch_Launch_UITests.swift
@@ -7,15 +7,18 @@
 
 import XCTest
 
+
 final class Winch_Launch_UITests: XCTestCase {
+
+    let app = XCUIApplication()
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        setupSnapshot(app)
+        app.resetAuthorizationStatus(for: .location)
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDownWithError() throws {
@@ -24,11 +27,21 @@ final class Winch_Launch_UITests: XCTestCase {
 
     func testExample() throws {
         // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        setupSnapshot(app)
         app.launch()
+                
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        _ = addUIInterruptionMonitor(withDescription: "Location Permission Alert") { (alertElement) -> Bool in
+            alertElement.buttons["Allow Once"].tap()
+            return true
+        }
 
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        snapshot("0Launch")
+        snapshot("Initial")
+        app.buttons["Airplane Mode"].tap()
+        snapshot("Settings")
+        app.buttons["Replay Off"].tap()
+        app.staticTexts["k2_apollonia_strong_wind_1"].tap()
+        app.buttons["Left"].tap()
+        sleep(50)
+        snapshot("Simulation")
     }
 }
