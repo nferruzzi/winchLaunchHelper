362b429706ea707829cfe0070b83a2c5e87d159a
Nicola Ferruzzi <nicola.ferruzzi+github@gmail.com>
2023-09-21 12:29:21 +0200

    Add fastlane configuration

---
+++ Winch Launch UITests/Winch_Launch_UITests.swift
@@ -0,0 +1,34 @@
+//
+//  Winch_Launch_UITests.swift
+//  Winch Launch UITests
+//
+//  Created by Nicola Ferruzzi on 21/09/23.
+//
+
+import XCTest
+
+final class Winch_Launch_UITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
+
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        snapshot("0Launch")
+    }
+}
---
+++ Winch Launch UITests/Winch_Launch_UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Winch_Launch_UITestsLaunchTests.swift
+//  Winch Launch UITests
+//
+//  Created by Nicola Ferruzzi on 21/09/23.
+//
+
+import XCTest
+
+final class Winch_Launch_UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
